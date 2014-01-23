2a2f0b305b25b46607c604c6c8d6f70b61f281a8
Danielle Smith <miss.d.smith@gmail.com>
2014-01-23 17:17:16 +0000

    Changed function to mimic gains set using method specified by Mulligan (1988)

--- anaglyphSetParams.m
+++ anaglyphSetParams.m
@@ -4,14 +4,20 @@ function [rgbParamsL,rgbParamsR] = anaglyphSetParams(stereoMode)
 % A small utility script that can be run to determine individualised colour
 % channel gains for anaglyph stereo. In order to be able to do this
 % on-the-fly, the Psychtoolbox imaging pipeline is enabled. It first 
-% callibrates the left eye and then the right eye. A stereoscopic rectangle
-% is then shown after gains have been set for both eyes. Finally, the gains 
-% for each eye are reported.
+% callibrates the left eye and then the right eye. Finally, the gains 
+% for each eye are reported. Gains are set using the method specified in
+% Mulligan (1988).
 %
 % Press the 'q', 'w' and 'e' keys to increase and the 'a', 's' and 'd' keys
 % to decrease the gain settings for the red, green and blue channels 
 % respectively. Once crosstalk has been minimalised between the eyes, press
 % the spacebar to move on to the next callibration.
+%
+% When adjusting the gains for the left filter (first instance), view through
+% right filter - adjust gains until you only see a horizontal contour, then
+% press space. The program will move on to adjusting gains for the right
+% filter (second instance) - view through the left filter and adjust gains
+% until you only see a vertical contour. Again, press space when finished.
 %
 % Optional parameters:
 % 'stereoMode' specifies the type of stereo display algorithm to use:
@@ -27,6 +33,8 @@ function [rgbParamsL,rgbParamsR] = anaglyphSetParams(stereoMode)
 %
 % Author:
 % Danielle Smith - lpxds5 at nottingham.ac.uk
+
+close all; clear all;
 
 Screen('Preference', 'SkipSyncTests', 1);
 
@@ -50,66 +58,71 @@ downb = KbName('d');
 space = KbName('space');
 escapeKey = KbName('ESCAPE');
 
+[keyboardIndices] = GetKeyboardIndices;
+keyboardIndex = keyboardIndices(2);
 
 % Specify screen res
 res = [1024 1280];
-
-% Let's try the PsychImaging pipeline
-PsychImaging('PrepareConfiguration');
-
-% Specify colours
-grey = [190 190 190];
-white = [255 255 255];
-black = [0 0 0];
 
 % Define the centre
 xcen = res(1)/2;
 ycen = res(2)/2;
 
+% Let's try the PsychImaging pipeline
+PsychImaging('PrepareConfiguration');
+
 % Set interval to 1
 leftGainSetting=0;
 rightGainSetting=0;
-rgbParamsL = [1.0 0.0 0.0]; % Default red parameters
-rgbParamsR = [0.0 0.5 0.5]; % Default cyan parameters
- 
-% How many screens? Check for use of stereomode
-screens=Screen('Screens');
-screenNumber=max(screens);
+rgbParamsL = [0.0 0.5 0.5]; % Default cyan parameters
+rgbParamsR = [1.0 0.0 0.0]; % Default red parameters
 
-% Open my screen: (open, window, color, size, color-bits, buffer, stereomode,
-% multisample-mode)
-% Open double-buffered onscreen window with the requested stereo mode:
-[windowPtr, windowRect]=PsychImaging('OpenWindow', screenNumber, grey, [], [], [], stereoMode);
+% Get the screen numbers, then draw to external screen.
+screens = Screen('Screens');
+screenNumber = max(screens);
+
+% Define colors (white will be 1 and black 0). This is because
+% in general luminace values are defined between 0 and 1 with 255 steps in
+% between. All values in Psychtoolbox are defined between 0 and 1
+white = WhiteIndex(screenNumber);
+grey = white / 2;
+
+% Open an on screen window using PsychImaging and color it grey.
+[windowPtr]=PsychImaging('OpenWindow', screenNumber, grey, [], [], [], stereoMode);
 HideCursor;
 
 % Show cleared start screen:
 Screen('Flip', windowPtr);
         
-% Drawing rectangle
-rect1 = [xcen-50, ycen-100, xcen+25, ycen+100];
-rect2 = [xcen-25, ycen-100, xcen+50, ycen+100];
+% Drawing rectangle (clockwise from top-left)
+rectWhite = [xcen-100, ycen-100, xcen, ycen];
+rectRed = [xcen, ycen-100, xcen+100, ycen];
+rectGrey = [xcen, ycen, xcen + 100, ycen+100];
+rectCyan = [xcen-100, ycen, xcen, ycen+100];
+rectMatrix = vertcat(rectWhite,rectRed,rectGrey,rectCyan)'; % matrix of rects for 'FillRect' command
+rectColors = horzcat(ones(3,2)*white,ones(3,1)*grey,ones(3,1)*white); % matrix of rect colours (white, white, grey, white clockwise from top-left)
 
 % Timing and stimulus presentation loop
 SetAnaglyphStereoParameters('LeftGains', windowPtr, rgbParamsL);
 SetAnaglyphStereoParameters('RightGains', windowPtr, rgbParamsR);
-[keyIsDown,secs,keyCode] = KbCheck;
 keyCode=zeros(1,256);
 keyIsDown=0;
 
-% Show the test stimulus until finish key is pressed
 while leftGainSetting == 0
-    (keyIsDown || (~keyCode(upr) & ~keyCode(downr) & ~keyCode(upg) & ~keyCode(downg) & ~keyCode(upb) & ~keyCode(downb)));
-    [keyIsDown,secs,keyCode] = KbCheck;
-    KbReleaseWait;
+    (keyIsDown (~keyCode(upr) & ~keyCode(downr) & ~keyCode(upg) & ~keyCode(downg) & ~keyCode(upb) & ~keyCode(downb)));
+    [keyIsDown,~,keyCode] = KbCheck(keyboardIndex);
+    KbReleaseWait(keyboardIndex);
     
     % Select left-eye image buffer for drawing:
     % Test stimulus
     Screen('SelectStereoDrawBuffer', windowPtr, 0);
-    Screen(windowPtr, 'FillRect', white, rect1); % White rect
+    Screen(windowPtr, 'FillRect', rectColors(:,[1,3,4]), rectMatrix(:,[1,3,4])); % White rect
 
     % Select right-eye image buffer for drawing:
     % Test stimulus
     Screen('SelectStereoDrawBuffer', windowPtr, 1);
+    Screen(windowPtr, 'FillRect', rectColors(:,[1,2,3]), rectMatrix(:,[1,2,3])); % White rect
+
     
     % Wait for response; could be any of seven keys
     if keyIsDown
@@ -139,18 +152,21 @@ keyIsDown = 0;
 
 % Show the test and ref stimulus for stim_time secs
 while rightGainSetting == 0
-    (keyIsDown || (~keyCode(upr) & ~keyCode(downr) & ~keyCode(upg) & ~keyCode(downg) & ~keyCode(upb) & ~keyCode(downb)));
-    [keyIsDown,secs,keyCode] = KbCheck;
-    KbReleaseWait;
+    (keyIsDown (~keyCode(upr) & ~keyCode(downr) & ~keyCode(upg) & ~keyCode(downg) & ~keyCode(upb) & ~keyCode(downb)));
+    [keyIsDown,~,keyCode] = KbCheck(keyboardIndex);
+    KbReleaseWait(keyboardIndex);
     
     % Select left-eye image buffer for drawing:
-    % Test stimulus 
+    % Test stimulus
     Screen('SelectStereoDrawBuffer', windowPtr, 0);
+    Screen(windowPtr, 'FillRect', rectColors(:,[1,3,4]), rectMatrix(:,[1,3,4])); % White rect
 
     % Select right-eye image buffer for drawing:
     % Test stimulus
     Screen('SelectStereoDrawBuffer', windowPtr, 1);
-    Screen(windowPtr, 'FillRect', white, rect2); % White rect
+    Screen(windowPtr, 'FillRect', rectColors(:,[1,2,3]), rectMatrix(:,[1,2,3])); % White rect
+    
+    % Wait for response; could be any of seven keys
     if keyIsDown
         if keyCode(upr)
             rgbParamsR(1) = rgbParamsR(1)+0.1;
@@ -174,29 +190,7 @@ while rightGainSetting == 0
    Screen(windowPtr,'Flip');
 end  % End of timed run
 
-keyIsDown = 0;
-
-% Show the test and ref stimulus for 5 secs
-tic
-while toc<5
-    % Select left-eye image buffer for drawing:
-    % Test stimulus
-    Screen('SelectStereoDrawBuffer', windowPtr, 0);
-    Screen(windowPtr, 'FillRect', white, rect1); % White rect
-
-    % Select right-eye image buffer for drawing:
-    % Test stimulus
-    Screen('SelectStereoDrawBuffer', windowPtr, 1);
-    Screen(windowPtr, 'FillRect', white, rect2); % White rect
-
-    % Now show it
-    Screen(windowPtr,'Flip');
-end  % End of timed run
-
-% Now turn all the expt screens off
-Screen('CloseAll');
+% Clear the screen.
+sca;
 ShowCursor;
 ListenChar(1);
-
-% We're done.
-return;
